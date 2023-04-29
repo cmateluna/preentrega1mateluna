7c3e60a150cace15dd2970c78ee45ea439117383
cmateluna <cmateluna@fundacioncchc.cl>
2023-04-29 18:30:59 -0400

    vol1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+//import Navbar from './components/NavBar/Navbar';
+import Container from './layout/Container';
+//import Contacto from './pages/Contacto';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    
+    <div className='App'>
+      <Container>
+        <Home />
+      </Container>
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <h1 className='border border-black border-1 text-center'>footer</h1>
+  )
+}
+
+export default Footer
